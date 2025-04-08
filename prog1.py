eba6fce84850a6566bc0a6c450be14de988b4f92
ruma847 <rumaambarish@gmail.com>
2025-04-08 21:47:22 +0530

    Add files via upload

--- prog1.py
+++ prog1.py
@@ -1,5 +1,17 @@
-print("Welcome to the world of programming!")
-print(4)
-print("\n Hello Python \n")
-print("hello", 5) 
-print("Welcome to ", end=  "a")     
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("data Type of name is", type(name))
+
+print("age :", age)
+print("data Type of age is", type(age))
+
+print("is_student :", is_student)
+print("data Type of is_student is", type(is_student))
+
+print("weight :", weight)
+print("data Type of weight is", type(weight))
+
